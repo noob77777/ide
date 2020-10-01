79137fc5c62d71e545184cc025c170fd191477b6
noob77777 <abhishekpal7@outlook.com>
2020-10-02 00:51:46 +0530

    Deployment v1.0

---
+++ precache-manifest.b8051cb6764157a0f87a162ce1aa348c.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "736cae96c9e34838c8b5546b59ff13a6",
+    "url": "/index.html"
+  },
+  {
+    "revision": "7b1d148583cbf9455832",
+    "url": "/static/css/main.5f361e03.chunk.css"
+  },
+  {
+    "revision": "81dca20364b7d5073c77",
+    "url": "/static/js/2.bee8a25d.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/static/js/2.bee8a25d.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "7b1d148583cbf9455832",
+    "url": "/static/js/main.59979e1a.chunk.js"
+  },
+  {
+    "revision": "e113d750196a4757e32c",
+    "url": "/static/js/runtime-main.48fcc6b3.js"
+  },
+  {
+    "revision": "5d5d9eefa31e5e13a6610d9fa7a283bb",
+    "url": "/static/media/logo.5d5d9eef.svg"
+  }
+]);
